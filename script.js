30e726c3d0c111d5222bcb7a9ee2666db8fb6a5e
Stan <skukhniy@gmail.com>
2021-07-27 17:42:29 +0100

    added hover function to change color of the grid blocks, and a clear button to remove the color

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 
 const container = document.querySelector("#container")
-
+// creates the 16x16 grid, and adds a hover event listener to change the grids color
 for (let i = 0; i<256; i++){
     const gridBlock = document.createElement('div')
+    gridBlock.classList.add('gridBlock')
+    gridBlock.addEventListener('mouseover',function(){
+        gridBlock.style.backgroundColor = "green"
+    })
     container.appendChild(gridBlock)
 }
+const clear = document.querySelector("#clear")
+//selects all the gridBlocks in one element
+const gridBlocks = document.querySelectorAll(".gridBlock")
+//iterates through each gridblock to remove style when button is pressed
+gridBlocks.forEach((gridBlock) => {
+    clear.addEventListener('click',function(){
+        gridBlock.removeAttribute("style")
+    })
+})
